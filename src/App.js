5eba624ef26ffabd76822ea743af299e3ed80876
Ajay-19 <itsajay4all@gmail.com>
2024-10-23 15:12:25 +0530

    code is updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Footer from './components/Footer';
+
 import Header from './components/Header';
-import Body from './components/Body'
+
 import { Outlet } from 'react-router-dom';
 
 function App() {
